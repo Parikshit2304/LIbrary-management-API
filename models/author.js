de55d73dc5634e18f7c007d134c2c10d36f245d2
Parikshit2304 <138897370+Parikshit2304@users.noreply.github.com>
2024-11-09 14:05:07 +0530

    Delete models directory

--- models/author.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const authorSchema = new mongoose.Schema({
-  name: String,
-  birthYear: Number,
-});
-
-module.exports = mongoose.model("Author", authorSchema);
